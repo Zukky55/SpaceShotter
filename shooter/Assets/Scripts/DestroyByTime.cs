8b39bc3aa82bcd5687f0ff6dc98b8d2c2414c1ae
Zukky55 <www.kase@gmail.com>
2018-08-06 19:36:21 +0900

    particle制御,Waveを繰り返す挙動追加
    coroutineの活用↓
    yield return new WaitWhile(() => ps.IsAlive(true));
    ps = particleSystemComponent
    particleSystem.IsAliveがfalseになったら削除

---
+++ shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    /// <summary></summary>
+    ParticleSystem ps;
+    
+    IEnumerator Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        yield return new WaitWhile(() => ps.IsAlive(true));
+        Destroy(gameObject);
+    }
+}
